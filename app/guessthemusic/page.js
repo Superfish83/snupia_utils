931ff061c54fb868b47169ab70a8c2b59f7e0604
Kim Yeonjun <kyjun050803@gmail.com>
2025-03-06 16:48:08 +0900

    동소제 에디션션

--- app/guessthemusic/page.js
+++ app/guessthemusic/page.js
@@ -15,20 +15,25 @@ export default function GuessTheMusic() {
   };
 
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center" style={{ marginTop: '10vh' }}>
-        <Image
-            src="/games/2.jpg"
-            alt="game2"
-            width={1080}
-            height={540}
-            className="w-80 rounded-xl mb-3"
-        />
-      <div className="mt-2" style={{ fontSize: 'clamp(1rem, 5vw, 6rem)' }}>
+    <div
+      className="w-full h-full flex flex-col items-center justify-center"
+      style={{ marginTop: "10vh" }}
+    >
+      <Image
+        src="/games/2.jpg"
+        alt="game2"
+        width={1080}
+        height={540}
+        className="w-80 rounded-xl mb-3"
+      />
+      <div className="mt-2" style={{ fontSize: "clamp(1rem, 5vw, 6rem)" }}>
         서울대학교 중앙 피아노 동아리 SNUPia
       </div>
       <div className="text-5xl my-2">피아노 음악 퀴즈</div>
       <div className="flex flex-col">
-        <Link href="/guessthemusic/game" className="systemBtn w-64 text-center">시작</Link>
+        <Link href="/guessthemusic/game" className="systemBtn w-64 text-center">
+          시작
+        </Link>
         <button
           className={`systemBtn w-64 text-center mt-1 transition-all duration-500 ${
             isSettingsClicked ? "bg-gray-400 text-gray-700 animate-shake" : ""
@@ -37,18 +42,38 @@ export default function GuessTheMusic() {
         >
           {isSettingsClicked ? "설정은 미구현입니다!" : "설정"}
         </button>
-        <Link href="/guessthemusic/ranking" className="systemBtn w-64 text-center mt-1">랭킹</Link>
+        <Link
+          href="/guessthemusic/ranking"
+          className="systemBtn w-64 text-center mt-1"
+        >
+          랭킹
+        </Link>
       </div>
+
+      <Link href="/" className="systemBtn w-64 text-center mt-1">
+        게임 목록
+      </Link>
+
       <div className="mx-auto mt-5 text-slate-400">
         웹게임 제작: SNUPia 정보부 김민재
       </div>
       <style jsx>{`
         @keyframes shake {
-          0% { transform: translateX(0); }
-          25% { transform: translateX(-5px); }
-          50% { transform: translateX(5px); }
-          75% { transform: translateX(-5px); }
-          100% { transform: translateX(0); }
+          0% {
+            transform: translateX(0);
+          }
+          25% {
+            transform: translateX(-5px);
+          }
+          50% {
+            transform: translateX(5px);
+          }
+          75% {
+            transform: translateX(-5px);
+          }
+          100% {
+            transform: translateX(0);
+          }
         }
         .animate-shake {
           animation: shake 0.5s ease-in-out;
